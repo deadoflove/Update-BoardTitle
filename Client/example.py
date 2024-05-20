5db7ad6b6da5e545abc606f2272b1936afd6d0cb
DeadOfLove <mt2atardecer@gmail.com>
2024-05-19 23:06:21 -0400

    example add

---
+++ Client/example.py
@@ -0,0 +1,18 @@
+
+	{
+		"name" : "Board",
+		"type" : "board_with_titlebar", 
+		"x" : 0, 
+		"y" : 0, 
+		"width" : 170, 
+		"height" : 110, 
+		"title" : "test text", 
+		#example edit
+		"color" : 0xffffffff, 
+		"text_x" : 0, 
+		"text_y" : 5,
+	},
+
+
+
+
